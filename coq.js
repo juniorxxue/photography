51cb103752139a0b0009ade17ff1af92d4a44ff3
Xu Xue <juniorxxue@gmail.com>
2021-10-09 18:50:20 +0800

    coq

---
+++ coq.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('pre').html(function(index, html){
+    return html.replace('Definition','<b>Definition</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('Fixpoint','<b>Fixpoint</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('match','<b>match</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('with','<b>with</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('end','<b>end</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('fix','<b>fix</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('fun','<b>fun</b>');
+});
+$('pre').html(function(index, html){
+    return html.replace('forall','<b>forall</b>');
+});
+});
